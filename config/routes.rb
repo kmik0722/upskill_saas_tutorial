45eff755e709c44c0a197f4587e445659f1d2063
Korey Mikolon <krmikolon@gmail.com>
2021-01-08 14:56:06 +0000

    attempting to fix spacing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+#Master list of the pages a user will be allowed to go to
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'pages#home'
+    get 'about', to: 'pages#about'
 end
